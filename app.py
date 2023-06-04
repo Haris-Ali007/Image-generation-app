33dd7d133129a5041ec6d586799760e4eec21296
root <root@DESKTOP-A7CQS11.localdomain>
2023-06-04 22:49:10 +0500

    Add generation function

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
 import gradio as gr
-# from image_generation import generate_image
-import numpy as np
-from PIL import Image
+from image_generation import generate_image
 
-def generate_image(prompt, model):
-    # Your model code here
-    # Use the prompt to generate an image
-    # Return the generated image
-    
-    # For demonstration purposes, we'll just return a random image
-    image = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
-    return Image.fromarray(image)
+dropdown_options = ["CompVis/stable-diffusion-v1-4", 
+                    "runwayml/stable-diffusion-v1-5",
+                    "stabilityai/stable-diffusion-2-1", 
+                    "stabilityai/stable-diffusion-2-1-base",
+                    "prompthero/openjourney-v4"]
 
-
-dropdown_options = ["model1", "model2", "model3"]
 # Define the categories and their corresponding prompts
-helper_prompts = {
-    "Clothes": [
-        "T-shirt",
-        "Jeans",
-        "Dress"
-    ],
-    "Cars": [
-        "Sedan",
-        "SUV",
-        "Sports car"
-    ],
-    "Food": [
-        "Pizza",
-        "Burger",
-        "Sushi"
-    ]
-}
+# helper_prompts = {
+#     "Clothes": [
+#         "T-shirt",
+#         "Jeans",
+#         "Dress"
+#     ],
+#     "Cars": [
+#         "Sedan",
+#         "SUV",
+#         "Sports car"
+#     ],
+#     "Food": [
+#         "Pizza",
+#         "Burger",
+#         "Sushi"
+#     ]
+# }
+
+# category = gr.Dropdown(choices=[""] + list(helper_prompts.keys()), label="Prompt category")
+# prompt = gr.Dropdown(label="Prompt")
+
+# def update_prompt(category_selected):
+#     if category_selected in helper_prompts:
+#         prompt.choices = helper_prompts[category_selected]
+#         prompt.interface.visible = True
+#     else:
+#         prompt.interface.visible = False
+
+# category.onchange = update_prompt
 
 def main():
     input = gr.Textbox(label="Enter your prompt")
@@ -39,26 +44,13 @@ def main():
                             label='Model',
                             info='Select model for inference')
 
-    category = gr.Dropdown(choices=[""] + list(helper_prompts.keys()), label="Prompt category")
-    prompt = gr.Dropdown(label="Prompt")
-
-    def update_prompt(category_selected):
-        if category_selected in helper_prompts:
-            prompt.choices = helper_prompts[category_selected]
-            prompt.interface.visible = True
-        else:
-            prompt.interface.visible = False
-
-    category.onchange = update_prompt
-
     output = gr.Image(label="Generated Image")
     app = gr.Interface(fn=generate_image, 
-                    inputs=[prompt, drop_down],
+                    inputs=[input, drop_down],
                     outputs=output, 
                     title="Logo Generation",
                     )
 
-    #TODO-> create a drop down for prompt selection
     app.launch()
 
 if __name__=="__main__":
--- image_generation.py
+++ image_generation.py
@@ -1,11 +1,16 @@
-from PIL import Image
-import numpy as np
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+import torch
+torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def generate_image(prompt, model):
-    # Your model code here
-    # Use the prompt to generate an image
-    # Return the generated image
-    
-    # For demonstration purposes, we'll just return a random image
-    image = np.random.randint(0, 255, size=(64, 64, 3), dtype=np.uint8)
-    return Image.fromarray(image)
+negative_prompt = "ugly, text, words, characters, tiling, poorly drawn hands, poorly drawn feet, \
+poorly drawn face, out of frame, extra limbs, disfigured, deformed, \
+body out of frame, bad anatomy, watermark, signature, cut off, low contrast, \
+underexposed, overexposed, bad art, beginner, amateur, distorted face."
+
+def generate_image(prompt, model_id):
+    model_id = "stabilityai/stable-diffusion-2-1"
+    scheduler = DPMSolverMultistepScheduler.from_pretrained(model_id, subfolder="scheduler")
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe = pipe.to(torch_device)
+    image = pipe(prompt, scheduler, num_inference_steps=30, negative_prompt=negative_prompt).images[0]  
+    return  image
