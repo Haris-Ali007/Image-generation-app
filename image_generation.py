ef90b93930f8c5284928974fc05e2b3c18c3aa22
Haris Ali <aliharris727@gmail.com>
2023-06-12 22:21:42 +0500

    Add scheduler configuration

--- image_generation.py
+++ image_generation.py
@@ -1,21 +1,34 @@
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler, PNDMScheduler, DPMSolverMultistepScheduler, LMSDiscreteScheduler, HeunDiscreteScheduler
 import torch
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
+schedulers = {
+    "EulerDiscreteScheduler": EulerDiscreteScheduler,
+    "PNDMScheduler": PNDMScheduler,
+    "DPMSolverMultistepScheduler": DPMSolverMultistepScheduler, 
+    "LMSDiscreteScheduler": LMSDiscreteScheduler,
+    "HeunDiscreteScheduler": HeunDiscreteScheduler,
+}
 
 negative_prompt = "ugly, text, words, characters, tiling, poorly drawn hands, poorly drawn feet, \
 poorly drawn face, out of frame, extra limbs, disfigured, deformed, \
 body out of frame, bad anatomy, watermark, signature, cut off, low contrast, \
 underexposed, overexposed, bad art, beginner, amateur, distorted face."
 
-def generate_image(prompt, model_id, classifier_guidance):
-    model_id = "stabilityai/stable-diffusion-2-1"
-    scheduler = DPMSolverMultistepScheduler.from_pretrained(model_id, subfolder="scheduler")
+def generate_image(prompt, 
+                    negative_prompt,
+                    model_id,
+                    scheduler_name,
+                    inference_steps,
+                    guidance_scale):
+
+    scheduler = schedulers[scheduler_name].from_pretrained(model_id, subfolder="scheduler")
     pipe = StableDiffusionPipeline.from_pretrained(model_id)
+    pipe.scheduler = scheduler
     pipe = pipe.to(torch_device)
     prompts = [prompt] * 4
     negative_prompts = [negative_prompt] * 4
-    images = pipe(prompts, num_inference_steps=30, negative_prompt=negative_prompts).images  
+    images = pipe(prompts, num_inference_steps=inference_steps, negative_prompt=negative_prompts, guidance_scale=guidance_scale).images  
     return  images
 
 
