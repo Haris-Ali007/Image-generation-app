82c43a84927c39cbf0048b7459d8fc648b4be160
Haris Ali <aliharris727@gmail.com>
2023-08-05 19:50:02 +0500

    Update app.py
    Set share flag to True

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def main():
                         outputs=gen_image_grid, 
                         title="Image Generation",
                         )
-        app.launch()
+        app.launch(True)
 
 if __name__=="__main__":
         main()
